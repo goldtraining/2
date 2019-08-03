329a52ab187e40087d59fdcaf0c1c26fd1675bb6
daniel <danielgold885@gmail.com>
2019-08-03 11:45:48 +0300

    1

--- src/test/java/class2.java
+++ src/test/java/class2.java
@@ -1,5 +1,10 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class class2 {
@@ -11,4 +16,17 @@ public class class2 {
 	  Thread.sleep(5000);
 	  driver.quit();
   }
+
+
+@Test
+public void f1() throws InterruptedException, MalformedURLException {
+	  
+	  DesiredCapabilities DC = new DesiredCapabilities();
+	  DC.setBrowserName("chrome");
+	  WebDriver driver = new RemoteWebDriver (new URL( "http://localhost:4444/wd/hub/" ), DC);
+	  System.out.println("Profile class!!!");
+	  Thread.sleep(5000);
+	  driver.quit();
 }
+}
+
